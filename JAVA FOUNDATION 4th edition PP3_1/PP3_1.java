0a98f68b749aa6f9d604beff852afcf9db240f1e
kaori <kaori55194@gmail.com>
2017-04-18 01:44:33 -0700

    first commit

---
+++ JAVA FOUNDATION 4th edition PP3_1/PP3_1.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class PP3_1 {
+
+    public static void main(String[] args) {
+	    String FN;
+	    String LN;
+		int FinalRandNum;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Whats your first name?");
+		FN = scan.nextLine();
+
+		System.out.print("Whats your last name?");
+		LN = scan.nextLine();
+
+		//create a Random Number object
+		Random RandomNum = new Random();
+		//Number object range from 10 to 99
+		FinalRandNum = RandomNum.nextInt(88)+10;
+
+	    System.out.print(FN.charAt(0)+LN.substring(0,5)+FinalRandNum);
+
+    }
+}
