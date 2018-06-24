16eadcb52ccadcadbd166a45827b75b9f2f4e3ed
kaori <kaori55194@gmail.com>
2018-06-24 14:43:39 -0700

    add hashmap

---
+++ Hashmap/src/Hashmap/Main.java
@@ -0,0 +1,21 @@
+package Hashmap;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<Integer,String> job = new HashMap<>();
+        job.put(1,"web_designer");
+        job.put(2,"mobile_developer");
+        job.put(3,"system_engineer");
+        job.put(4,"soft_engineer");
+
+        /// THE WAY GET VALUE
+        System.out.println(job.get(1));
+
+
+
+
+    }
+}
